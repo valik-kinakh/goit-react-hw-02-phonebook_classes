995b2e17fa55a37dafd184734d7ac3651195616e
Valik Kinakh <valik.kinah@gmail.com>
2022-11-21 22:12:43 +0200

    add contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import Title from './common/Title';
+import ContactForm from './ContactForm';
 
 class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'test', number: '227-91-26' },
+    ],
+    searchQuery: '',
+  };
+
+  addContact = contact => {
+    const { contacts } = this.state;
+    const duplicate = contacts.find(el => el.name === contact.name);
+    if (duplicate) {
+      alert(`${duplicate.name} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div className="app">
+        <Title title="Phonebook" />
+        <ContactForm addContact={this.addContact} />
+      </div>
+    );
   }
 }
 
